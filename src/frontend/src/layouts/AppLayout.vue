f670fa54653a0f3e166acb0482825376f13ac77f
alnusglutinosa <olga.app2@gmail.com>
2021-06-30 21:01:21 +0300

    Создание компонента Конструктор пиццы

---
+++ src/frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app-layout">
+    <IndexHome />
+  </div>
+</template>
+
+<script>
+import IndexHome from "@/views/Index.vue";
+
+export default {
+  name: "AppLayout",
+  components: {
+    IndexHome,
+  },
+};
+</script>
